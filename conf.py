87e584549cb7ee3d69899610c1245d3f4bcf1417
Raymond Liu <raymond.liu@intel.com>
2014-08-08 13:10:01 +0800

    modify theme

--- conf.py
+++ conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['rst2pdf.pdfbuilder']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -96,7 +96,7 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {'nosidebar': 'True'}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
